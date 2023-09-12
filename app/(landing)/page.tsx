f3b8101af1476e817ecede45e20dcb77b5b165cf
supra <supradipdey1@gmail.com>
2023-09-12 22:29:21 +0530

    new

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,20 +1,36 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+// import { Button } from "@/components/ui/button";
+// import Link from "next/link";
 
-import React from "react";
+import { LandingContent } from "@/components/custom/landing-content";
+import { LandingHero } from "@/components/custom/landing-hero";
+import { LandingNavbar } from "@/components/custom/landing-navbar";
+
+// import React from "react";
+
+// const LandingPage = () => {
+//   return (
+//     <div>
+//       LandingPage
+//       <div>
+//         <Link href="/sign-in">
+//           <Button>Login</Button>
+//         </Link>
+//         <Link href="/sign-up">
+//           <Button>Sign Up</Button>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LandingPage;
 
 const LandingPage = () => {
   return (
-    <div>
-      LandingPage
-      <div>
-        <Link href="/sign-in">
-          <Button>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Sign Up</Button>
-        </Link>
-      </div>
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
   );
 };
